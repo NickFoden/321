830a625303ba899732895204e744390eda15ffcf
Nick Foden <nickfoden@gmail.com>
2022-06-13 23:39:48 -0400

    Setup v1 (#2)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,24 @@
-export default function Index() {
+import { Link } from "react-router-dom";
+import styles from "../src/styles/global.css";
+
+export function links() {
+  return [{ rel: "stylesheet", href: styles }];
+}
+
+const IndexPage = () => {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="scrolling_container">
+      <div className="scrolling_logo" />
+        <p className="text body_text">
+        We at 321 Magazine aim to serve as a platform committed to fostering a deeper engagement with truly personal image making, while cultivating opportunities for both excellent next generation creatives as well as established professionals. As an editorial playground, we proudly display over 100 pages of content featuring photo stories, illustrations, writing, and focused evidence that answers our call to contributors for what each issue’s theme means specifically to them.
+        </p>
+        <nav className="contact_links">
+          <img src="/assets/LOGOMARK.svg" className="logo" alt="321"/>
+          <Link className="text contact_link" to="/contact">Contact</Link>
+          <a className="text contact_link" href="https://www.instagram.com/321_magazine" target="_blank" rel="noreferrer">Instagram</a>
+        </nav>
     </div>
   );
-}
+};
+
+export default IndexPage;
